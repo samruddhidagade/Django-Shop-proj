665f577fd8440c2cfc6124b1001124828d0fbfa6
samruddhidagade <dagadesamruddhi@gmail.com>
2022-11-02 20:36:17 +0530

    Shopsproject

---
+++ Shops/Shops/products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+# Create your views here.
+def index(request):
+ products = Product.objects.all()
+ return render(request,'index.html',{'products':products})
+ #return HttpResponse("<h1>Welcome</h1>")
+def about(request):
+ return HttpResponse("<h1>About Page</h1>")
